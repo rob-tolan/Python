f9200d03f374194cf930d1fc658fb66cb1627430
Robert Tolan <tolanro@tcd.ie>
2020-02-12 01:53:43 +0000

    Add files via upload

---
+++ functions.py
@@ -0,0 +1,11 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Wed Feb 12 01:40:00 2020
+
+@author: Owner
+"""
+def sum_n_integers(n):
+    return n*(n+1)/2
+for i in range(2,11):
+ print(sum_n_integers(i))
+
