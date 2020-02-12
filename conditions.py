ac57f282eb936bbaca67d689834697d6967a819b
Robert Tolan <tolanro@tcd.ie>
2020-02-12 01:33:07 +0000

    Add files via upload

---
+++ conditions.py
@@ -0,0 +1,18 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Mon Feb 10 11:27:33 2020
+
+@author: Owner
+"""
+Europe = ["Germany", "France", "Spain"]
+Asia = ["Vietnam", "China", "India"]
+country = "Spain"
+continent = ""
+if country in continent:
+    print(country,"in continent")
+else:
+    print(country,"not in continent")
+
+    
+
+
