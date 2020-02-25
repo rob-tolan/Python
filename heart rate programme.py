802e12dd9ecfce2078d6a365daa9fa3289b2e8ca
Robert Tolan <tolanro@tcd.ie>
2020-02-25 11:15:48 +0000

    Add files via upload

---
+++ heart rate programme.py
@@ -0,0 +1,13 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Tue Feb 25 11:11:17 2020
+
+@author: Owner
+"""
+age = int(input('Enter your age in years:))
+max_heart_rate: 206.9 -(0.67*age)
+target = 0.65*max_heart_rate
+print('Your target fat-burning heart rate is', target)
+
+
+
