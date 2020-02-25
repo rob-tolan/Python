28d8a25c92856a27eee43c2547b1422c769a4146
Robert Tolan <tolanro@tcd.ie>
2020-02-25 12:32:39 +0000

    Add files via upload

---
+++ list of squares of numbers.py
@@ -0,0 +1,10 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Tue Feb 25 12:28:57 2020
+
+@author: Owner
+"""
+squares = [k*k for k in range(1, n+1)]
+squares = []
+for k in range(1, n+1):
+    squares.append(k*k) 
