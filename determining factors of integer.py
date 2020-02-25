1f9f7dc99695ad21dddf81a0fe10db98888d25cd
Robert Tolan <tolanro@tcd.ie>
2020-02-25 11:20:10 +0000

    Add files via upload

---
+++ determining factors of integer.py
@@ -0,0 +1,12 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Tue Feb 25 11:15:48 2020
+
+@author: Owner
+"""
+def factors(n):
+ results =[]
+for k in range(1, n+1):
+ if n%k ==0:
+ results = append(k)
+return result
