4223614a862acda209324eb61882118edc0ef48b
Robert Tolan <tolanro@tcd.ie>
2020-02-25 12:26:12 +0000

    Add files via upload

---
+++ fibonacci progression.py
@@ -0,0 +1,15 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Tue Feb 25 12:23:00 2020
+
+@author: Owner
+"""
+
+def fibonacci():
+    a=0
+    b=1
+while True:
+    yield a
+future = a+b
+a=b
+   while k < sqrt(n) 
